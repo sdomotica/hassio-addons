990cc4d4db0c802f24de8826bfb40dbdc562cf92
Sandro Di Marco <sandrodimarco@libero.it>
2026-06-04 14:51:38 +0200

    fix rw

--- opensprinkler-new/run.sh
+++ opensprinkler-new/run.sh
@@ -5,5 +5,6 @@ echo " Avvio di OpenSprinkler (Pre-compilato Debian 64-bit)"
 echo " Client MQTT integrato e pronto."
 echo "====================================================="
 
-# Lancio dell'eseguibile principale
-exec /app/OpenSprinkler
+# -d indica la cartella dei dati di configurazione (/data è persistente in HassOS)
+# -s indica la cartella dei file statici web (se integrati nell'eseguibile, altrimenti punta a /app)
+exec /app/OpenSprinkler -d /data
